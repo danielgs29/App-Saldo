b316766b394dee8cccb29593f83e55491cf4743e
danielgs29 <danielgs2929@gmail.com>
2022-08-25 23:57:50 +0200

    first html, js

---
+++ js/App.js
@@ -0,0 +1,22 @@
+const ingresos = [
+    new Ingreso("salario",2100),
+    new Ingreso("Venta Coche",200000)
+]
+
+const egresos = [
+    new Egreso("Renta",900),
+    new Egreso("Ropa",400)
+]
+
+let cargarApp=()=>{
+    cargarCabecero();
+}
+
+let totalIngresos=()=>{
+    let totalIngreso=0;
+    
+}
+
+let cargarCabecero=()=>{
+
+}
